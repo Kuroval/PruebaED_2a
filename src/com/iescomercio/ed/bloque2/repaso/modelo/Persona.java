f11f80b0a097f22df67d90f1d6e627a1d01c0b86
Kuroval <Lozancroc@gmail.com>
2022-03-03 06:10:42 +0100

    documentada Persona

--- src/com/iescomercio/ed/bloque2/repaso/modelo/Persona.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/Persona.java
@@ -1,33 +1,56 @@
 package com.iescomercio.ed.bloque2.repaso.modelo;
 
+/**
+ * Clase que representa una persona como una cadena dni que es el dni de la persona, una cadena nombre que es el nombre/nombres 
+ * de la persona y apellido1 que es el primer apellido de la persona
+ * @author Carlos Saenz Adan
+ * @version 1.0
+ * @since 3.0
+ * @see Curso
+ */
+
 public class Persona{
-	
+	/**
+	 * Cadena que representa el dni de una persona
+	 */
 	private String dni;
+	/**
+	 * Cadena que representa el nombre/nombres de una persona
+	 */
 	private String nombre;
+	/**
+	 * Cadena que representa el primer apellido de una persona
+	 */
 	private String apellido1;
 	
 	public Persona() {}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Constructor que crea un objeto del tipo persona con los parametros introducidos como parametro
+	 * @param dni dni de la persona a crear
+	 * @param nombre nombre/nombres de la persona a crear
+	 * @param apellido1 primer apellido de la persona a crear
+	 */
 	public Persona(String dni, String nombre, String apellido1) {
 		this.dni = dni;
 		this.nombre = nombre;
 		this.apellido1 = apellido1;
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Funcion que devuelve el dni de la persona
+	 * @return Devuelve una cadena dni que es el dni de la persona
+	 */
 	public String getDni() {
 		return dni;
 	}
 	
-	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	//Lo comento como si estuviese bien
+	/**
+	 * Accion que cambia el valor del dni de una persona si el dni acaba en letra
+	 * @param dni cadena dni que sustituira al antiguo dni
+	 * @throws Exception "El ultimo caracter introducido no es una letra"
+	 */
 	public void setDni(String dni) throws Exception {
 		//comprobacion de si el ultimo caracter es una letra
 		if(Character.isLetter(dni.charAt(dni.length()))) {
@@ -37,30 +60,34 @@ public class Persona{
 		}
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Funcion que devuelve el nombre de la persona
+	 * @return Devuelve una cadena nombre que es el nombre de la persona
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Accion que cambia el valor del nombre de una persona 
+	 * @param nombre cadena por la que se cambiara el antiguo nombre de la persona
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Funcion que devuelve el primer apellido de la persona
+	 * @return Devuelve una cadena apellido1 que es el primer apellido de la persona
+	 */
 	public String getApellido1() {
 		return apellido1;
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Accion que cambia el valor del primer apellido de una persona
+	 * @param apellido1 cadena por la que se cambiara el antiguo primer apellido de la persona
+	 */
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
 	}
