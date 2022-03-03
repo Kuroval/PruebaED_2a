45a01cf5a9861e38796a36bc703c15597ecc3442
Kuroval <Lozancroc@gmail.com>
2022-03-03 06:21:21 +0100

    documentada Curso

--- src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
@@ -3,13 +3,24 @@ package com.iescomercio.ed.bloque2.repaso.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que representa un curso como una lista de instancias de la clase Persona llamada listaAlumnos
+ * @author Carlos Saenz Adan
+ * @version 1.0
+ * @since 3.0
+ * @see Persona
+ */
 public class Curso {
-	
+	/**
+	 * Lista de objetos persona
+	 */
 	private List<Persona> listaAlumnos;
 
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Accion que elimina un alumno de listaAlumnos si el dni tiene 9 digitos alfanumericos y existe en la lista
+	 * @param dni Cadena alfanumerica de 9 digitos dni del alumno a eliminar de la lista
+	 * @throws Exception "El dni no tiene la longitud adecuada"
+	 */
 	public void eliminarAlumno(String dni) throws Exception {
 		if(dni.length()==9) {//comprobar la longitud del dni
 			listaAlumnos.remove(new Persona(dni, "", "")); //solo hace falta el dni
@@ -18,16 +29,21 @@ public class Curso {
 		}
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Accion que aniade un alumno a la lista de alumnos y por tanto al curso
+	 * @param p Persona a aniadir al curso
+	 */
 	public void aniadirAlumno(Persona p) {
 		listaAlumnos.add(p);
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Funcion que devuelve verdadero en caso de que el dni introducido por parametro se encuentre en el curso y
+	 * falso en caso contrario
+	 * @param dni Cadena alfanumerica de 9 digitos dni del alumno a aniadir a la lista
+	 * @return Devuelve verdadero en caso de que el dni introducido por parametro se encuentre en el curso y
+	 * falso en caso contrario
+	 */
 	public Boolean estaRegistrado(String dni) {
 		int i =0;
 		Boolean encontrado=false;
@@ -40,16 +56,17 @@ public class Curso {
 		return encontrado;
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Constructor que inicializa un curso como una lista vacia de alumnos
+	 */
 	public Curso() {
 		listaAlumnos= new ArrayList<Persona>();
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Funcion que duvuelve el numero de personas en un curso
+	 * @return Devuelve un entero que es el numero de alumnos en el curso
+	 */
 	public Integer numeroAlumnos() {return listaAlumnos.size();}
 	
 	
