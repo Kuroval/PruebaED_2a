386c7a3688e095d6742ad3f26d0cdc2061e3672d
Kuroval <Lozancroc@gmail.com>
2022-03-03 05:41:50 +0100

    implementacion metodo BeforeEachCurso

--- src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
@@ -2,10 +2,19 @@ package com.iescomercio.ed.bloque2.repaso.modelo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CursoTest {
 
+	private Curso c;
+	
+	@BeforeEach
+	void metodoBeforeEachCurso() {
+		c=new Curso();
+		c.aniadirAlumno(new Persona("12345678A","Juan","Vivaldi"));
+	}
+	
 	@Test
 	void testEliminarAlumno() {
 		fail("Not yet implemented");
