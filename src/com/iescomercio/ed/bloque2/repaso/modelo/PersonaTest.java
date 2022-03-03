b7ea50dd601bc184b7ee2dd188722c930a1f21b5
Kuroval <Lozancroc@gmail.com>
2022-03-03 05:07:56 +0100

    implementacion test testPersonaStringStringString

---
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/PersonaTest.java
@@ -0,0 +1,49 @@
+package com.iescomercio.ed.bloque2.repaso.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.iescomercio.ed.junit.Persona;
+
+class PersonaTest {
+	
+	@Test
+	void testPersonaStringStringString() {
+		Persona p= new Persona("12345678A","Perico","Palotes");
+		assertEquals("12345678A", p.getDni());
+		assertEquals("Perico", p.getNombre());
+		assertEquals("Palotes", p.getApellido1());
+	}
+
+	@Test
+	void testGetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApellido1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApellido1() {
+		fail("Not yet implemented");
+	}
+
+}
